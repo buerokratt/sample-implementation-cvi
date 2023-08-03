66c8262a18fa6147460aa2e9b5298f9ad28e67d7
Marko <marko.kull@hotmail.com>
2023-08-03 11:25:06 +0300

    Added main navigation to base layout. Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/layout.tsx';
 import WelcomePage from './pages/info/welcome';
+import MainNavigationPage from './pages/component/main-navigation';
 
 
 const App: FC = () => {
@@ -11,6 +12,7 @@ const App: FC = () => {
         <Route element={<Layout />}>
           <Route index element={<Navigate to="/info/welcome" />} />
           <Route path="/info/welcome" element={<WelcomePage />} />
+          <Route path="/component/mainnavigation" element={<MainNavigationPage />} />
         </Route>
       </Routes>
   );
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,54 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import MainNavigation from 'components/main-navigation/main-navigation.tsx'
 import './layout.scss';
 
 const Layout: FC = () => {
+  /** Main navigation START */
+  /** Impement logic to load/request main navifation items */
+  const navmenu: any = [
+    {
+        "id": "info",
+        "label": {
+            "et": "Info",
+            "en": "Info"
+        },
+        "path": "/info",
+        "children": [
+            {
+                "label": {
+                    "et": "Tervitus",
+                    "en": "Welcome"
+                },
+                "path": "/info/welcome"
+            }
+        ]
+    },
+    {
+      "id": "component",
+      "label": {
+          "et": "Komponendid",
+          "en": "Components"
+      },
+      "path": "/component",
+      "children": [
+          {
+              "label": {
+                  "et": "Navigatsiooni menüü",
+                  "en": "Main navigation"
+              },
+              "path": "/component/mainnavigation"
+          }
+      ]
+    }
+  ];
+
+
+  /** Main navigation END */
 
   return (
     <div className='layout'>
-      <div id='placeholder_for_main_navigation'>Main navigation</div>
+      <MainNavigation items={navmenu} />
       <div className='layout__wrapper'>
         <div id='placeholder_for_header'> Header</div>
         <main className='layout__main'>
