d6e20e94e63dd221fcadaceda2908f8cfe17d1d2
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-03-25 16:48:23 +0200

    chore(1244): Enhanced Online-Offline-Idle Flow/ Increased idle timeout

--- src/headerV2/src/components/Dialog/index.tsx
+++ src/headerV2/src/components/Dialog/index.tsx
@@ -1,40 +1,63 @@
-import React, { FC, PropsWithChildren, ReactNode } from 'react';
-import * as RadixDialog from '@radix-ui/react-dialog';
-import { MdOutlineClose } from 'react-icons/md';
-import clsx from 'clsx';
+import React, { FC, PropsWithChildren, ReactNode } from "react";
+import * as RadixDialog from "@radix-ui/react-dialog";
+import { MdOutlineClose } from "react-icons/md";
+import clsx from "clsx";
 
-import Track from '../Track';
-import Icon from '../Icon';
-import './Dialog.scss';
+import Track from "../Track";
+import Icon from "../Icon";
+import "./Dialog.scss";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 type DialogProps = {
   title?: string | null;
   footer?: ReactNode;
   onClose: () => void;
-  size?: 'default' | 'large';
-}
+  outSideDismiss?: boolean;
+  size?: "default" | "large";
+};
 
-const Dialog: FC<PropsWithChildren<DialogProps>> = ({ title, footer, onClose, size = 'default', children }) => {
+const Dialog: FC<PropsWithChildren<DialogProps>> = ({
+  title,
+  footer,
+  onClose,
+  size = "default",
+  children,
+  outSideDismiss = true,
+}) => {
   return (
     <RadixDialog.Root defaultOpen={true} onOpenChange={onClose}>
       <RadixDialog.Portal>
-        <RadixDialog.Overlay className='dialog__overlay' />
-        <RadixDialog.Content className={clsx('dialog', `dialog--${size}`)}>
-          {
-            title && <div className='dialog__header'>
-              <RadixDialog.Title className='h3 dialog__title'>{title}</RadixDialog.Title>
+        <RadixDialog.Overlay className="dialog__overlay" />
+        <RadixDialog.Content
+          className={clsx("dialog", `dialog--${size}`)}
+          aria-describedby={title ?? undefined}
+          onInteractOutside={(e) => {
+            if (!outSideDismiss) e.preventDefault();
+          }}
+          onEscapeKeyDown={(e) => {
+            if (!outSideDismiss) e.preventDefault();
+          }}
+        >
+          {title && (
+            <div className="dialog__header">
+              <RadixDialog.Title className="h3 dialog__title">{title}</RadixDialog.Title>
               <RadixDialog.Close asChild>
-                <button className='dialog__close'>
-                  <Icon icon={<MdOutlineClose />} size='medium' />
+                <button className="dialog__close">
+                  <Icon icon={<MdOutlineClose />} size="medium" />
                 </button>
               </RadixDialog.Close>
             </div>
-          }
-          <div className='dialog__body'>
-            {children}
-          </div>
+          )}
+          {!title && (
+            <VisuallyHidden>
+              <RadixDialog.Title className="h3 dialog__title">{title}</RadixDialog.Title>
+            </VisuallyHidden>
+          )}
+          <div className="dialog__body">{children}</div>
           {footer && (
-            <Track className='dialog__footer' gap={16} justify='end'>{footer}</Track>
+            <Track className="dialog__footer" gap={16} justify="end">
+              {footer}
+            </Track>
           )}
         </RadixDialog.Content>
       </RadixDialog.Portal>
