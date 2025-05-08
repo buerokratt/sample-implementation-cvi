d3da3674460fb32c85da3f9d47515c7f0f5fc31b
Vassili Moskaljov <112167412+ExiRain@users.noreply.github.com>
2025-05-08 15:08:30 +0300

    Increase base popup duration to 5 seconds (#64)

--- src/headerV2/src/constants/config.ts
+++ src/headerV2/src/constants/config.ts
@@ -3,7 +3,7 @@ export const WELCOME_MESSAGE_LENGTH = 250;
 export const STATUS_COMMENT_LENGTH = 100;
 export const USER_IDLE_STATUS_TIMEOUT = 900000; // milliseconds - 15 minutes
 export const CHAT_INPUT_LENGTH = 500;
-export const TOAST_TIMEOUT = 2000;
+export const TOAST_TIMEOUT = 5000;
 export const CHAT_HISTORY_PREFERENCES_KEY = 'chat-history-preferences';
 
 export const isHiddenFeaturesEnabled = 
